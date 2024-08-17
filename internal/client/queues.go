8a6f799081b219ea60927382e5efe122551f9b0f
Stefan Pfaffel <s.pfaffel@gmail.com>
2024-08-17 09:14:55 +0200

    chore(client): add api client

---
+++ internal/client/queues.go
@@ -0,0 +1,66 @@
+package client
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+const resourceName string = "queues"
+
+func sendAndUnmarshal[T any](c *Client, requestOptions RequestOptions) (*T, error) {
+	body, err := c.executeRequest(requestOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	response := new(T)
+	err = json.Unmarshal(body, &QueueReponse{response})
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
+
+func (c *Client) GetQueue(queueId string) (*Queue, error) {
+	requestOptions := RequestOptions{
+		Method:       http.MethodGet,
+		Path:         fmt.Sprintf("/%s/%s", resourceName, queueId),
+		ExpectStatus: http.StatusOK,
+	}
+
+	return sendAndUnmarshal[Queue](c, requestOptions)
+}
+
+func (c *Client) CreateQueue(newQueue Queue) (*Queue, error) {
+	requestOptions := RequestOptions{
+		Body:         newQueue,
+		Method:       http.MethodPost,
+		Path:         fmt.Sprintf("/%s", resourceName),
+		ExpectStatus: http.StatusOK,
+	}
+
+	return sendAndUnmarshal[Queue](c, requestOptions)
+}
+
+func (c *Client) UpdateQueue(queueId string, updatedQueue Queue) (*Queue, error) {
+	requestOptions := RequestOptions{
+		Body:         updatedQueue,
+		Method:       http.MethodPut,
+		Path:         fmt.Sprintf("/%s/%s", resourceName, queueId),
+		ExpectStatus: http.StatusOK,
+	}
+
+	return sendAndUnmarshal[Queue](c, requestOptions)
+}
+
+func (c *Client) DeleteQueue(queueId string) (*Queue, error) {
+	requestOptions := RequestOptions{
+		Method:       http.MethodDelete,
+		Path:         fmt.Sprintf("/%s/%s", resourceName, queueId),
+		ExpectStatus: http.StatusOK,
+	}
+
+	return sendAndUnmarshal[Queue](c, requestOptions)
+}

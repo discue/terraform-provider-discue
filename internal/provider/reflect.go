2941e78c8fef68afa52516bd5e695ce859f165bf
Stefan Pfaffel <s.pfaffel@gmail.com>
2024-08-27 08:59:09 +0200

    chore(reflect): improve error handling

--- internal/provider/reflect.go
+++ internal/provider/reflect.go
@@ -5,34 +5,34 @@ import (
 	"reflect"
 )
 
-func getValueOf[T any](itf interface{}, key string) T {
+func getValueOf[T any](itf interface{}, key string) (T, error) {
 	if !checkFieldExists(itf, key) {
 		var result T
-		return result
+		return result, fmt.Errorf("field %s not found", key)
 	}
 
-	field := getFieldByName(itf, key, true)
+	field := getFieldByName(itf, key)
 
 	if field.IsValid() && field.CanInterface() {
-		return field.Interface().(T)
-	} else {
-		fmt.Printf("Field %s not found or inaccessible\n", key)
+		return field.Interface().(T), nil
 	}
+
 	var result T
-	return result
+	return result, fmt.Errorf("field %s inaccessible", key)
 }
 
-func setValueOf(itf interface{}, key string, value any) {
-	field := getFieldByName(itf, key, false)
+func setValueOf(itf interface{}, key string, value any) error {
+	field := getFieldByName(itf, key)
 
 	if field.IsValid() && field.CanSet() {
 		field.Set(reflect.ValueOf(value))
+		return nil
 	} else {
-		fmt.Printf("Field %s not found or inaccessible\n", key)
+		return fmt.Errorf("field %s not found or inaccessible", key)
 	}
 }
 
-func getFieldByName(itf interface{}, key string, ignoreInvalid bool) reflect.Value {
+func getFieldByName(itf interface{}, key string) reflect.Value {
 	v := reflect.ValueOf(itf)
 	if v.Kind() == reflect.Ptr {
 		v = v.Elem()
